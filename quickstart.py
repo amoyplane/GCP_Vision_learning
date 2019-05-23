adeff7e0d13b3c8bf469cde7725da8e192192dc9
李玮祺 <2016011291@secoder.net>
2019-05-23 10:55:12 +0800

    2

--- quickstart.py
+++ quickstart.py
@@ -50,6 +50,20 @@ def sizefilter(vertices):
     return False
 
 
+def expand(vertices):
+	t_vertices = copy.deepcopy(vertices)
+	t_vertices[0].x = t_vertices[0].x - 20
+	t_vertices[1].x = t_vertices[1].x + 20
+	t_vertices[2].x = t_vertices[2].x + 20
+	t_vertices[3].x = t_vertices[3].x - 20
+
+    t_vertices[0].y = t_vertices[0].y - 5
+	t_vertices[1].y = t_vertices[1].y - 5
+	t_vertices[2].y = t_vertices[2].y + 5
+	t_vertices[3].y = t_vertices[3].y + 5
+
+	return t_vertices	
+
 def run_quickstart(file_name):
 
     # Instantiates a client
@@ -89,10 +103,19 @@ def run_quickstart(file_name):
             f.write('bounds: {} \n'.format(','.join(vertices)))
 
             # block.bounding_box.vertices[1].x
+            '''
             draw.drawline((block.bounding_box.vertices[0].x, block.bounding_box.vertices[0].y), (block.bounding_box.vertices[1].x, block.bounding_box.vertices[1].y), (0, 255, 0))
             draw.drawline((block.bounding_box.vertices[1].x, block.bounding_box.vertices[1].y), (block.bounding_box.vertices[2].x, block.bounding_box.vertices[2].y), (0, 255, 0))
             draw.drawline((block.bounding_box.vertices[2].x, block.bounding_box.vertices[2].y), (block.bounding_box.vertices[3].x, block.bounding_box.vertices[3].y), (0, 255, 0))
             draw.drawline((block.bounding_box.vertices[3].x, block.bounding_box.vertices[3].y), (block.bounding_box.vertices[0].x, block.bounding_box.vertices[0].y), (0, 255, 0))
+            '''
+
+            tbound = expand(block.bounding_box.vertices)
+            draw.drawline((tbound[0].x, tbound[0].y), (tbound[1].x, tbound[1].y), (0, 255, 0))
+            draw.drawline((tbound[1].x, tbound[1].y), (tbound[2].x, tbound[2].y), (0, 255, 0))
+            draw.drawline((tbound[2].x, tbound[2].y), (tbound[3].x, tbound[3].y), (0, 255, 0))
+            draw.drawline((tbound[3].x, tbound[3].y), (tbound[0].x, tbound[0].y), (0, 255, 0))
+
 
             for paragraph in block.paragraphs:
                 # f.write('Paragraph confidence: {}'.format(
