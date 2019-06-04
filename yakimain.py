0f9c7c8f7c828b10a14262554a54aec70be34918
李玮祺 <2016011291@secoder.net>
2019-06-04 18:24:59 +0800

    3

---
+++ yakimain.py
@@ -0,0 +1,20 @@
+import quickstart as qs
+import drawline as draw
+
+
+def doall(infile, outfile):
+	f = open('ans.txt', 'w')
+    # file_name='/root/pic/t3.jpg'
+    file_name = infile
+    draw.openpic(file_name)
+    # os.system("export GOOGLE_APPLICATION_CREDENTIALS=\"/root/mykey.json\"")
+    rst = qs.run_quickstart(file_name)
+    draw.writepic(outfile)
+    f.close()
+
+    return rst
+
+
+
+if __name__ == '__main__':
+	ret = doall('/root/pic/' + sys.argv[1], 'pro_' + sys.argv[1])
